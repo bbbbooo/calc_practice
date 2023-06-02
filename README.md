7351f0a3a0a7ede15e39fe27078fe251f20d23d5
bbbbooo <qlqlzbxkzl@naver.com>
2023-06-02 16:54:46 +0900

    자바 계산기 연습

---
+++ README.md
@@ -0,0 +1,68 @@
+# 자바 계산기 연습
+
+---
+> - `boolean` 값을 통해 `while문`을 통제할 수 있다.
+> - `try`, `catch`를 통해 예외처리
+> - 잘못된 값을 입력했을 경우, 무한 반복
+
+
+
+
+1. Scanner를 통해 값을 입력을 받는다.
+
+![img.png](img.png)
+
+
+2. 정상적으로 값을 받으면 결과 출력 후 종료
+
+![img_1.png](img_1.png)
+
+
+3. 잘못된 값을 입력 받았으면 오류 메세지 출력 후 재입력
+
+![img_2.png](img_2.png)
+
+<br>
+
+---
+
+### 예외 상황
+
+1. 정수 입력이 아닌 실수, 문자, 문자열을 입력했을 경우
+> - `InputMismatchException` 발생 <br>
+> - `try ~ catch`를 통해 예외처리
+
+<br>
+
+2. 정수에 `0`을 입력했고 사칙 연산이 `/` 혹은 `%`를 입력했을 경우
+> - `ArithmeticException` 발생 <br>
+> - `try ~ catch`를 통해 예외처리
+
+<br>
+
+3. 사칙 연산 입력이 아닌 어떠한 다른 값을 입력했을 경우
+> - `checkZero` 메소드를 통해 0인지 아닌지 판별함
+> - 0일 경우 `ArithmeticException` 발생
+
+<br>
+
+### 예외 상황에 대한 처리
+
+- `true` 값을 가진 `con` 전역 변수 생성
+- `try~catch`를 `while`로 감싼 형태의 코드를 작성
+```java
+while(con){
+    try{
+        ...
+        con = false;
+        }
+    catch(Exception e){
+        ...
+        in.nextLine();
+        }
+}
+```
+- `catch`에 진입 시, 예외상황 대한 처리를 해주고 `in.nextLine();`을 통해 입력 버퍼를 비워 `while문`이 무한히 반복되는 현상을 방지했다. 
+- 모든 예외 상황을 문제없이 통과하면 `con = false;`로 반복문이 종료되게끔 구성했다.
+
+
---
+++ src/main/java/com/practice/app1.java
@@ -0,0 +1,82 @@
+package com.practice;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class app1 {
+    private boolean con;
+
+    public static void main(String[] args) {
+        System.out.println("계산기 시작...");
+
+        app1 app = new app1();
+
+        Scanner in = new Scanner(System.in);
+        app.con = true;
+
+        while (app.con) {
+             try{
+                System.out.print("첫번째 정수 : ");
+                int a = in.nextInt();
+
+                System.out.print("두번째 정수 : ");
+                int b = in.nextInt();
+
+                System.out.print("사칙연산 입력(+,-,*,/,%) : ");
+                char c = in.next().charAt(0);
+
+                app.calc(a, b, c);
+                app.con = false; // while문 종료
+            }
+
+            catch (InputMismatchException e) {
+                System.out.println("------------------------");
+                System.out.println("잘못된 값이 입력되었습니다.");
+                System.out.println("다시 입력해주세요.");
+                System.out.println("------------------------");
+
+                in.nextLine(); // 입력버퍼에 값이 남아있으므로 비워줘야 함. 이 코드가 없으면 위의 문장들이 무한히 반복됨
+             }
+
+             catch (ArithmeticException e){
+                 System.out.println("------------------------");
+                 System.out.println("0으로는 나눌 수 없습니다.");
+                 System.out.println("다시 입력해주세요.");
+                 System.out.println("------------------------");
+
+                 in.nextLine();
+             }
+        }
+
+    }
+
+    // 계산 메소드. / 와 %는 둘 중 하나라도 0일 경우 예외처리
+    public void calc(int a, int b, char c) {
+        switch(c) {
+            case '+':
+                System.out.println(a+b);
+                break;
+            case '-':
+                System.out.println(a-b);
+                break;
+            case '*':
+                System.out.println(a*b);
+            case '/':
+                checkZero(a, b);
+                System.out.println(a/b);
+            case '%':
+                checkZero(a,b);
+                System.out.println(a%b);
+        }
+    }
+
+    // a,b가 0일 경우 예외처리 하는 메소드
+    private static void checkZero(int a, int b) {
+        if(a ==0 || b ==0){
+            throw new ArithmeticException();
+        }
+    }
+
+
+}
+
